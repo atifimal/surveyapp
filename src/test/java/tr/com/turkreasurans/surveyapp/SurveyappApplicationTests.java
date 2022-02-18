7076bae1fda18fc0e8a0d793f9e22c64a2b5a6fc
TURKRE\atif.imal <aimal@turkre.com>
2022-02-18 16:17:01 +0300

    Inıtialize project

---
+++ src/test/java/tr/com/turkreasurans/surveyapp/SurveyappApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.turkreasurans.surveyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
